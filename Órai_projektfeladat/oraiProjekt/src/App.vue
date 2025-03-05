6494ed2b92bf48399296989a605e5f1033499f3d
b0totdav@vasvari.org <b0totdav@vasvari.org>
2025-03-05 14:13:36 +0100

    All files created

---
+++ Órai_projektfeladat/oraiProjekt/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+ 
+      <nav>
+        <RouterLink to="/">Kezdőoldal</RouterLink>
+        <RouterLink to="/reserve">Foglalás</RouterLink>
+      </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
