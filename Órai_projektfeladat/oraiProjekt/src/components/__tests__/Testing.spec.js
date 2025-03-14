3ac8be4b9d9d2ce248016e1fcb2d22702a22e9e8
b0totdav@vasvari.org <b0totdav@vasvari.org>
2025-03-14 08:50:47 +0100

    2 teszt megvan

--- Órai_projektfeladat/oraiProjekt/src/components/__tests__/Testing.spec.js
+++ Órai_projektfeladat/oraiProjekt/src/components/__tests__/Testing.spec.js
@@ -1,10 +1,23 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
+import HomeView from '../../views/HomeView.vue'
+import ReserveView from '@/views/ReserveView.vue'
+import { useDateStore } from '@/stores/dates.js'
+import { createPinia, setActivePinia } from 'pinia'
 
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+describe('Táblázat', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('Tartalom ellenőrzése', () => {
+    const wrapper = mount(HomeView)
+    expect(wrapper.text()).toContain('Péntek')
+  })
+
+  it('Összes sor megjelenik?', () => {
+    const wrapper = mount(HomeView)
+    expect(wrapper.text()).toContain('15:00')
   })
 })
